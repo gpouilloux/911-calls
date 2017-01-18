95c907867564bab9654e052149cb2feedae366af
Guillaume Pouilloux <gui.pouilloux@gmail.com>
2017-01-18 16:48:23 +0100

    [elasticsearch] import into ES and queries

--- elasticsearch/README.md
+++ elasticsearch/README.md
@@ -22,7 +22,74 @@ GET <nom de votre index>/_count
 À vous de jouer ! Écrivez les requêtes ElasticSearch permettant de résoudre les problèmes posés.
 
 ```
-TODO : ajouter les requêtes ElasticSearch ici
+Mapping fields
+PUT 911/_mapping/call
+{
+  "call": {
+    "properties": {
+      "category": {
+        "type": "text",
+        "fielddata": true
+      },
+      "source": {
+        "properties": {
+          "location": {
+            "type": "geo_point"
+          }
+        }
+      }
+    }
+  }
+}
+
+GET 911/call/_search
+{
+  "size": 0,
+  "aggs": {
+    "group_by_category": {
+      "terms": {
+        "field": "category.keyword"
+      }
+    }
+  }
+}
+
+{
+  "took": 14,
+  "timed_out": false,
+  "_shards": {
+    "total": 5,
+    "successful": 5,
+    "failed": 0
+  },
+  "hits": {
+    "total": 153194,
+    "max_score": 0,
+    "hits": []
+  },
+  "aggregations": {
+    "group_by_category": {
+      "doc_count_error_upper_bound": 0,
+      "sum_other_doc_count": 0,
+      "buckets": [
+        {
+          "key": "EMS",
+          "doc_count": 75589
+        },
+        {
+          "key": "Traffic",
+          "doc_count": 54549
+        },
+        {
+          "key": "Fire",
+          "doc_count": 23056
+        }
+      ]
+    }
+  }
+}
+
+
 ```
 
 ## Kibana
--- elasticsearch/import.js
+++ elasticsearch/import.js
@@ -1,17 +1,53 @@
 var elasticsearch = require('elasticsearch');
 var csv = require('csv-parser');
 var fs = require('fs');
+var splitArray = require('split-array');
 
 var esClient = new elasticsearch.Client({
   host: 'localhost:9200',
   log: 'error'
 });
 
+var calls = [];
 fs.createReadStream('../911.csv')
     .pipe(csv())
     .on('data', data => {
-      // TODO extract one line from CSV
+      // extract one line from CSV
+      var [category, title] = data.title.split(': ');
+      var call = {
+        latitude: data.lat,
+        longitude: data.lng,
+        location : { lon: parseFloat(data.lng), lat: parseFloat(data.lat) },
+        description: data.desc,
+        zip: data.zip,
+        category: category,
+        title: title,
+        district: data.twp,
+        address: data.addr,
+        timestamp: new Date(data.timeStamp)
+      };
+      calls.push(call);
     })
     .on('end', () => {
-      // TODO insert data to ES
+      // insert data to ES
+      var chunks = splitArray(calls, 130000);
+      chunks.forEach(chunk => {
+        var esBody = [];
+        chunk.forEach(c => {
+          var index = {
+            index: {
+              _index: '911',
+              _type: 'call'
+            }
+          };
+          esBody.push(index);
+          esBody.push(c);
+        });
+        esClient.bulk({
+          body: esBody
+        }, function (err, resp) {
+          console.log(resp);
+          console.log(`Error: ${err}`);
+        });
+      });
     });
